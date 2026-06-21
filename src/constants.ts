498e4c6660c73ca7156fa88ab810f39fdc552f6f
mydkong <contact.donkeykong@protonmail.com>
2026-06-21 14:46:31 +0200

    Adding some emojis

--- src/constants.ts
+++ src/constants.ts
@@ -19,3 +19,29 @@ export const GIFS = {
 		"https://static.klipy.com/ii/d7aec6f6f171607374b2065c836f92f4/82/07/qkmQXuax.gif"  // https://klipy.com/gifs/ping-pong-9
 	]
 }
+
+export const REGEX = {
+	MODRINTH_PROJECT_ID: /^[\w!@\$\(\)`.+,"\-']{3,64}$/
+}
+
+export const EMOJIS = {
+	TRUE: "<:true:1518215170080637009>",
+	FALSE: "<:false:1518215169032065074>",
+	BETWEEN: "<:between:1518217136785588367>",
+	OFF: "<:off:1518234441212891206>",
+	TRUE_JOIN: "<:true_join:1518234443385540620>",
+	TRUE_JOIN_END: "<:true_join_end:1518234442450206871>",
+	FALSE_JOIN: "<:false_join:1518234438633525429>",
+	FALSE_JOIN_END: "<:false_join_end:1518234439933624400>",
+	OFF_JOIN: "<:off_join:1518234437173772439>",
+	OFF_JOIN_END: "<:off_join_end:1518234436053762108>",
+	FABRIC_ICON: "<:fabric_icon:1517475209139978363>",
+	QUILT_ICON: "<:quilt_icon:1517476031286607945>",
+	FORGE_ICON: "<:forge_icon:1517476878242287626>",
+	NEOFORGE_ICON: "<:neoforge_icon:1517496107587080233>",
+	PAPERMC_ICON: "<:papermc_icon:1517497490054578177>",
+	PURPUR_ICON: "<:purpur_icon:1517498713738707106>",
+	SPIGOT_ICON: "<:spigot_icon:1517498983248035993>",
+	FOLIA_ICON: "<:folia_icon:1517499610900332607>",
+	BUKKIT_ICON: "<:bukkit_icon:1517499608798859425>"
+}
