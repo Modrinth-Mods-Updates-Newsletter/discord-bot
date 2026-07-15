11d02c0259f992b7a225893bc36781db7333d9bb
Poulpe Glacé <contact.donkeykong@protonmail.com>
2026-07-15 08:36:39 +0200

    clean code!

--- gen-service-file.ts
+++ gen-service-file.ts
@@ -4,6 +4,8 @@ import template from './mmundiscbot.service.template' with { type: 'text' }
 const dir = import.meta.dirname
 const tempPath = 'mmundiscbot.service.template'
 
-const outputfile = fs.writeFileSync('mmundiscbot.service', template.replace('{bunDir}', process.execPath).replaceAll('{dir}', dir))
+const outputfile = fs.writeFileSync('mmundiscbot.service', template
+  .replace('{bunDir}', process.execPath)
+  .replaceAll('{dir}', dir))
 
 console.info('Service file successfully generated')
